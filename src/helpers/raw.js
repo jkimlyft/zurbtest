d1beecd46846a686b3553030c533852f6a7c07b9
John Kim <jkim@jkim-mbp114.sfhq.lyft-corp.net>
2017-03-09 12:18:49 -0800

    create initial folder

--- src/helpers/raw.js
+++ src/helpers/raw.js
@@ -8,3 +8,14 @@
 module.exports = function(content) {
   return content.fn();
 }
+
+var parentSource = $("#parent-template").html(),
+    partialSource = $("#partial-template").html(),
+    parentTemplate = Handlebars.compile(parentSource),
+    partialTemplate = Handlebars.compile(partialSource);
+
+Handlebars.registerPartial({
+  partialTemplate: partialSource
+});
+
+$('#content').html(parentTemplate);
